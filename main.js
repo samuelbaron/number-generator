151f53b7f22948475e6fae078dd3248f814a659b
samuelbarin <samuel.baron.pl@gmail.com>
2020-10-25 16:54:09 +0100

    Finished

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
-const LoremIpsum = require("lorem-ipsum");
-console.log(LoremIpsum)
+const minimal = document.querySelector(".minimal")
+const maximal = document.querySelector(".maximal")
+let minimalValue = ""
+let maximalValue = ""
+let result = ""
+
+const print = () => {
+  if ( isNaN(result)) {
+    return 0
+  }
+  else {
+    let resultParagraph = document.querySelector(".result-number")
+    resultParagraph.innerHTML = result
+  }
+}
+
+const generate = () => {
+  if (isNaN(minimalValue) || (isNaN(maximalValue)) ) return
+  const min = parseInt(minimalValue)
+  const max = parseInt(maximalValue)
+  if (max < min) {
+    alert("Maximal number must be greater than a minimal one")
+    return
+  }
+  result = (Math.floor(Math.random() * ( max - min + 1 ) + min ))
+  print()
+}
+
+minimal.addEventListener("keyup", () => {
+  let value = minimal.value
+  if ( value !== minimalValue )  {
+    minimalValue = minimal.value
+    generate()
+  }
+})
+maximal.addEventListener("keyup", () => {
+  let value = maximal.value
+  if ( value !== maximalValue )  {
+    maximalValue = value
+    generate()
+  }
+})
